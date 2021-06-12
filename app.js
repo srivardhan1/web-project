0bc44099360dbc84656adcc54a58e3d80d5f20c6
Chilukamarri Srivardhan <srivardhanchilukamarri@gmail.com>
2021-06-12 23:40:39 +0530

    User Model and DB Bootstrapping done

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dbconnect=require('./backend/lib/connectLib');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+require('./backend/lib/dbUsersBootstrap').createUsers();
 
+var app = express();
+dbconnect.connect();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
