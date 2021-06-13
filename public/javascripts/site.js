e550c3c6d2171c489948d661a04273c22e2f172d
Chilukamarri Srivardhan <srivardhanchilukamarri@gmail.com>
2021-06-13 11:52:26 +0530

    Login API

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,4 +1,42 @@
 $(function(){
     console.log("Jquery Running");
-    toastr.info('Toastr Running')
-})
+    // toastr.success('Toastr Running')
+
+    var loggedIn = false;
+    if(loggedIn){
+        console.log("Logged In");
+        $("#signedIn").show();
+        $("#notSignedIn").hide();
+    }
+    else{
+        console.log("Not Logged In");
+        $("#notSignedIn").show();
+        $("#signedIn").hide();
+    }
+
+    // On click of login button, make AJAX call.
+    $("#btnLogin").on('click', function(){
+        var userObj = {username: '', password:''};
+        userObj.username = $("#txtUserName").val();
+        userObj.password = $("#txtPassword").val();
+        $.post( "/api/login", userObj)
+        .done(function( data ) {
+            alert( "Data Loaded: " + JSON.stringify(data) );
+            if(data.success){
+                toastr.success('Login Successful');
+                // front end session management
+                $("#signedIn").show();
+                $("#notSignedIn").hide();
+            }
+            else{
+                toastr.success('Login Failed');
+            }
+        })
+        .fail(function() {
+            //alert( "error" );
+        })
+        .always(function() {
+            //alert( "finished" );
+        });
+    })
+}) 
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const userLib=require('../backend/lib/userLib');
 
 /* GET users listing. */
 router.post('/login', function(req, res) {
 
     // DB
 
-    var responseJson = {success: true, message: 'Login Failed', user: null };
-    res.json(responseJson);
+    //var responseJson = {success: true, message: 'Login Failed', user: null };
+    userLib.isUserValid(req.body,function(resultJson){
+    res.json(resultJson);
+})
 });
 
 module.exports = router;
