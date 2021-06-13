89a5e82fee5a696184c70ef497f17a149c28ae6f
Chilukamarri Srivardhan <srivardhanchilukamarri@gmail.com>
2021-06-13 12:52:45 +0530

    Configuration of Session On Server Side

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,26 @@ router.post('/login', function(req, res) {
 
     
     userLib.isUserValid(req.body,function(resultJson){
+    if(resultJson.success==true){
+    console.log("Session for User Initialized");
+    req.session.user = {username: resultJson.username}; // Init session object for this user
+    }
     res.json(resultJson);
 })
+});
+// Dummy API that will return rol of current user who has requested mailbox --- Through SESSION
+router.get('/mailbox', function(req, res) {
+    // if this is a valid user having session, return his mailbox
+    if(req.session && req.session.user){
+        // User is authenticated and his session table has an entry, so we know his username
+        // Can we get his details from db?
+        // Can we make a db call using userLib or mailboxLib to get his data and return back?
+        res.json({success:true, username:req.session.user.username, 'mailbox': req.session.user.username});
+    }
+    else{
+        // Redirect him to login page
+        res.json({success:false, message:'Need to be logged in to get mailbox'});
+    }
 });
 
 module.exports = router;
